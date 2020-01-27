6a5b2df5debf82cac80fa02f6a5aa9e9b187be0e
kicktv <khaledtv29@gmail.com>
2020-01-28 00:47:44 +0100

    Update gd.php

--- gd.php
+++ gd.php
@@ -1,5 +1,6 @@
 <?php
 $id= $_GET['id'];
 $gdrive = "https://www.googleapis.com/drive/v3/files/" .$id. "?alt=media&key=AIzaSyAgJNu0Gt0vAFVCN3g0fqlRrksIrscRiLM";
+header('Content-Type: application/octet-stream');
 readfile($gdrive);    
 ?>
